a2574a3b7cb1444c4be8a358183b4ca50cab2e7f
ahmdfqhhhhh <ahmadfaqih335@gmail.com>
2026-07-02 20:59:37 +0700

    fix: sesuaikan variabel db js dengan env indonesia di vercel

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,9 +2,9 @@ const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    user: process.env.PENGGUNA_DB,        // <-- Diubah ke variabel Vercel lu
+    password: process.env.KATA_SANDI_DB,  // <-- Diubah ke variabel Vercel lu
+    database: process.env.NAMA_DB         // <-- Diubah ke variabel Vercel lu
 });
 
 connection.connect((err) => {
