46796bc7e113c66cc90f22c23dee4e8ab5bc5425
ahmdfqhhhhh <ahmadfaqih335@gmail.com>
2026-07-02 21:16:22 +0700

    fix: ganti koneksi database ke variabel asli clever cloud

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.PENGGUNA_DB,        // <-- Diubah ke variabel Vercel lu
-    password: process.env.KATA_SANDI_DB,  // <-- Diubah ke variabel Vercel lu
-    database: process.env.NAMA_DB         // <-- Diubah ke variabel Vercel lu
+    host: process.env.HOST_ADDON_MYSQL,
+    user: process.env.PENGGUNA_ADDON_MYSQL,
+    password: process.env.KATA_SANDI_ADDON_MYSQL,
+    database: process.env.MYSQL_ADDON_DB,
+    port: process.env.PORT_ADDON_MYSQL || 3306
 });
 
 connection.connect((err) => {
